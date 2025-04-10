4199f957937134988f062e27a56318b1140ec73d
Kevin wasonga <kevinwasonga415@outlook.com>
2025-04-10 05:32:16 +0300

    post

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link>
-  </nav>
+  <NavBar />
   <router-view />
 </template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
 
 <style>
 #app {
--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,19 +1,20 @@
 <template>
     <div class="create">
-        <form>
+        <form @submit.prevent="handleSubmit">
             <label>Title:</label>
             <input v-model="title" type="text" required>
             <label>Content:</label>
             <textarea v-model="body" required></textarea>
             <label>Tag (hit enter to add a tag)</label>
-            <input @keydown.enter.prevent="" type="text">
-            <input type="submit" value="Add post">
+            <input @keydown.enter.prevent="handleTagEntry" type="text" v-model="tag">
+            <input type="submit" value="Add post" class="button">
         </form>
     </div>
 </template>
 
 <script>
 import { ref } from 'vue';
+// import { useRouter } from 'vue-router';
 
 export default {
     name: "CreateView",
@@ -21,14 +22,97 @@ export default {
         const title = ref('');
         const body = ref('');
         const tag = ref('');
-        const tags = ref([]);
+        const tags = [];
 
-        // const 
+        // const router = useRouter();
+
+        const handleTagEntry = () => {
+            if (!tags.includes(tag.value)) {
+                tag.value = tag.value.trim();
+                tags.push(tag.value)
+            }
+
+            tag.value = ''
+        }
+
+        const handleSubmit = () => {
+            const post = { title: title.value, body: body.value, tags: tags }
+
+            async function submitPost() {
+                try {
+                    const res = await fetch('http://localhost:3000/posts', {
+                        method: 'POST',
+                        headers: { "Content-Type": 'application/json' },
+                        body: JSON.stringify(post)
+                    })
 
 
-        return { title, body, tag, tags }
+
+                    if (res.ok) console.log("Send to home route");
+
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+
+            submitPost()
+        }
+
+        return { title, body, tag, handleTagEntry, handleSubmit }
     }
 }
 </script>
 
-<style></style>
+<style>
+form {
+    max-width: 480px;
+    margin: 0 auto;
+    text-align: left;
+}
+
+input,
+textarea {
+    display: block;
+    margin: 10px 0;
+    width: 100%;
+    box-sizing: border-box;
+    padding: 10px;
+    border: 1px solid #eee;
+}
+
+textarea {
+    height: 160px;
+}
+
+label {
+    display: inline-block;
+    margin-top: 30px;
+    position: relative;
+    font-size: 20px;
+    color: white;
+    margin-bottom: 10px;
+}
+
+label::before {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 100%;
+    background: #ff8800;
+    position: absolute;
+    z-index: -1;
+    padding-right: 40px;
+    left: -30px;
+    transform: rotateZ(-1.5deg);
+}
+
+.button {
+    display: block;
+    margin-top: 30px;
+    background: #ff8800;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    font-size: 18px;
+}
+</style>
